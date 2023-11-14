5fc52c9907b4af0747bad7b08324467205686ff5
Retro21 <56453764+jekabso21@users.noreply.github.com>
2023-11-14 04:59:30 +0200

    init push

---
+++ client/main.lua
@@ -0,0 +1,79 @@
+local tabEnabled = false
+local tabLoaded = true
+photoUrl = nil
+
+RegisterCommand('docs', function(source, args, raw)
+    TriggerEvent('codecraft:openMdt', args[1])
+end)
+
+
+function NUIFOCUSFN(state, url)
+    SetNuiFocus(state, state)
+    if state == true then
+        SendNUIMessage({ showtab = true, src = GetPlayerServerId(PlayerId()), url = url})
+    else
+        SendNUIMessage({ hidetab = true, src = GetPlayerServerId(PlayerId()), url = url})
+    end
+    return bool
+end
+
+
+RegisterNUICallback("NUI:close", function()
+    tabEnabled = false
+    NUIFOCUSFN(false)
+    stopAnim()
+end)
+
+RegisterNetEvent("codecraft:openMdt")
+AddEventHandler("codecraft:openMdt", function(url)
+    tabEnabled = not tabEnabled
+    if tabEnabled then
+        startAnim()
+    else
+        stopAnim()
+    end
+    NUIFOCUSFN(tabEnabled, url)
+
+end)
+
+function startAnim()
+    Citizen.CreateThread(function()
+        RequestAnimDict("amb@world_human_seat_wall_tablet@female@base")
+        while not HasAnimDictLoaded("amb@world_human_seat_wall_tablet@female@base") do
+            Citizen.Wait(0)
+        end
+        attachObject()
+        TaskPlayAnim(GetPlayerPed(-1), "amb@world_human_seat_wall_tablet@female@base", "base", 8.0, -8.0, -1, 50, 0, false, false, false)
+    end)
+end
+
+function attachObject()
+    tab = CreateObject(GetHashKey("prop_cs_tablet"), 0, 0, 0, true, true, true)
+    AttachEntityToEntity(tab, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 57005), 0.17, 0.10, -0.13, 20.0, 180.0, 180.0, true, true, false, true, 1, true)
+end
+
+function stopAnim()
+    StopAnimTask(GetPlayerPed(-1), "amb@world_human_seat_wall_tablet@female@base", "base", 8.0, -8.0, -1, 50, 0, false, false, false)
+    DeleteEntity(tab)
+end
+
+function startAnim()
+    Citizen.CreateThread(function()
+        RequestAnimDict("amb@world_human_seat_wall_tablet@female@base")
+        while not HasAnimDictLoaded("amb@world_human_seat_wall_tablet@female@base") do
+            Citizen.Wait(0)
+        end
+        attachObject()
+        TaskPlayAnim(GetPlayerPed(-1), "amb@world_human_seat_wall_tablet@female@base", "base", 8.0, -8.0, -1, 50, 0, false, false, false)
+    end)
+end
+
+function attachObject()
+    tab = CreateObject(GetHashKey("prop_cs_tablet"), 0, 0, 0, true, true, true)
+    AttachEntityToEntity(tab, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 57005), 0.17, 0.10, -0.13, 20.0, 180.0, 180.0, true, true, false, true, 1, true)
+end
+
+function stopAnim()
+    StopAnimTask(GetPlayerPed(-1), "amb@world_human_seat_wall_tablet@female@base", "base", 8.0, -8.0, -1, 50, 0, false, false, false)
+    DeleteEntity(tab)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Retro21'
+description 'Simple tool to allow players open google sheets in game.'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+}
+
+
+client_script {
+    "client/main.lua"
+}
